bba08eedbcfc990a6d4bf6f1d9c8b8bd2b1700ff
DangVinhKhiem <khiemdangvinh8@gmail.com>
2026-01-21 20:06:33 +0700

    SV1: Khoi tao du an va lop Book

---
+++ src/Book.java
@@ -0,0 +1,16 @@
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+
+    public Book(int id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + " | Title: " + title + " | Author: " + author;
+    }
+}
